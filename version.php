d9bf2ee37a32bb9a594177108bd87dfb8b5194cc
lazydaisy <lazydaisy@visible-expression.co.uk>
2016-02-13 22:43:41 +0000

    Updated Version: theme_gologo - fixed typo

--- version.php
+++ version.php
@@ -24,7 +24,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'theme_gologo';
 $plugin->maturity     = MATURITY_STABLE;
-$plugin->release      = '3.0 (Build: 201602013)';
+$plugin->release      = '3.0 (Build: 20160213)';
 $plugin->requires  = 2016012900;
 $plugin->version   = 2016021300;
 $plugin->dependencies = array(
