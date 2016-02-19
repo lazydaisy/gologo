c4619de3d95f220fda32587d0d9b94b143444afa
lazydaisy <lazydaisy@visible-expression.co.uk>
2016-02-19 12:00:07 +0000

    Issue No:002 theme_gologo: Added javascript for slider to config.php.

--- config.php
+++ config.php
@@ -41,6 +41,8 @@ $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->editor_sheets = array();
 $THEME->enable_dock = true;
 
+$THEME->javascripts_footer = array('gologo');
+
 $THEME->layouts = array(
     // Most backwards compatible layout without the blocks - this is the layout used by default.
     'base' => array(
